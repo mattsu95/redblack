3868a4e0c349e33a7db8df386db93d25163f3f13
Matheus Seghatti <matheusseghatti@gmail.com>
2025-10-07 18:26:37 -0300

    correções no que foi feito até agora e algumas alterações possivelmente desnecessárias

--- rbtree.cpp
+++ rbtree.cpp
@@ -16,22 +16,28 @@ public:
 
     Node *root;
     Dictionary(){root = NULL;}
+    Node* Insert(const string &w, const string &m) { //achei legal isso aqui pra garantir raiz sempre preta
+        Insertrb(w, m, root);                        //mas não queria usar mais de um ponteiro pra node
+        //InsertFixup(n);                            //então fiz essa gambiarra desnecessária aí mesmo
+        root->color = false;
+    }
 
-    Node* Insert(const string &w, const string &m, Node *n) {
+private:
+    Node* Insertrb(const string& w, const string& m, Node *n) {
         if(!n) {//node == NULL
-            Node *aux = new Node(w, m);
-            //return new Node(w, m); //n sei se ta certo isso
-            aux->color = false; //leaf node always black
-            return aux; //faz sentido?
+            return new Node(w, m); //n sei se ta certo isso
         }
 
         if(w > n->word) {
-            n->right = Insert(w, m, n->right);
+            n->right = Insertrb(w, m, n->right);
+            n->right->parent = n;
         }
-        else if(w < n->word) 
-            n->left = Insert(w, m, n->right);
-        else
-            return n;  
+        else if(w < n->word) {
+            n->left = Insertrb(w, m, n->left);
+            n->left->parent = n;
+        }
+
+        return n;  
         
     }
 };
