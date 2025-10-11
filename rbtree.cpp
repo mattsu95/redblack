299e8ab7210d3ca6a3f189ecb088969f493857cd
Matheus Seghatti <matheusseghatti@gmail.com>
2025-10-11 19:11:43 -0300

    descobri que não dá pra só copiar o insert da avl, então corrigi e comecei a troca de cores

--- rbtree.cpp
+++ rbtree.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+#define black false
+#define red true
+
 struct Node {
     string word, meaning;
     Node *left, *right, *parent;
-    bool color; //true for red, false for black
+    bool color; //true == red; false == black
 
-    Node(string w, string m) : word(w), meaning(m), left(nullptr), right(nullptr), parent(nullptr), color(true) {}
+    Node(string w, string m) : word(w), meaning(m), left(nullptr), right(nullptr), parent(nullptr), color(red) {}
 };
 
 class Dictionary {
@@ -16,28 +19,59 @@ public:
 
     Node *root;
     Dictionary(){root = NULL;}
-    Node* Insert(const string &w, const string &m) { //achei legal isso aqui pra garantir raiz sempre preta
-        Insertrb(w, m, root);                        //mas não queria usar mais de um ponteiro pra node
-        //InsertFixup(n);                            //então fiz essa gambiarra desnecessária aí mesmo
-        root->color = false;
+    void Insert(const string &w, const string &m) {
+        Node* z = new Node(w, m);
+        Node* n = Insertrb(z);
+        
+        //InsertFixup(n);                           
+        root->color = black;
     }
 
 private:
-    Node* Insertrb(const string& w, const string& m, Node *n) {
-        if(!n) {//node == NULL
-            return new Node(w, m); //n sei se ta certo isso
-        }
+    Node* Insertrb(Node *z) {
+        Node* x = root;         //não teve jeito, tive que fazer isso
+        Node* y = nullptr;
 
-        if(w > n->word) {
-            n->right = Insertrb(w, m, n->right);
-            n->right->parent = n;
+        while(x) {
+            y = x;
+            if(z->word < x->word) x = x->left;
+            else if(z->word > x->word) x = x->right;
+            else {
+                delete z;
+                return x;
+            }
         }
-        else if(w < n->word) {
-            n->left = Insertrb(w, m, n->left);
-            n->left->parent = n;
-        }
+        z->parent = y;
+        if(!y) root = z;
+        else if(z->word < y->word) y->left = z;
+        else y->right = z;
 
-        return n;  
+        return z;  
+    }
+
+    void InsertFixup(Node* z) {
+        Node* y = z->parent;
+        Node* g = y->parent;      //g de godfather
+        Node* uncle = nullptr;
+        
+        if(!y || g) return; //se for a raiz ou um filho direto da raiz ta tudo certo, pq aparentemente a regra de nó folha ser sempre preto serve pra nada
+
+        while(y && y->color) {
+            uncle = (g->left == y)? g->right: g->left;
+            if(uncle && uncle->color) { //1st case - z's uncle red
+                uncle->color = black;   //o tio e o pai pegam a negritude do avô
+                y->color = black;
+                g->color = red;
+            }
+
+
+            //aqui, implementar o resto dos casos
+
+
+            z = y->parent;
+            y = z->parent;
+            g = y->parent;
+        }
         
     }
 };
